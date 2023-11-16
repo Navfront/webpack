4c266a17dd763617eb575822a718b1b5fdc65b3a
Nazhiganov AV <b3rt@yandex.ru>
2023-11-16 21:36:50 +0300

    add dev-server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,35 +1,52 @@
-import { resolve as _resolve } from "path";
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { ProgressPlugin } from 'webpack';
+import { resolve as _resolve, join as _join } from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { ProgressPlugin } from 'webpack'
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
+import type { Configuration } from 'webpack'
 
 interface EnvVariables {
-    mode: 'development' | 'production'
+  mode: 'development' | 'production'
+  port: number
 }
 
-export default (env: EnvVariables)=> ({
+export default (env: EnvVariables): Configuration & DevServerConfiguration => {
+  const isDev = env.mode === 'development'
+
+  return {
     mode: env.mode ?? 'development',
     entry: _resolve(__dirname, 'src', 'index.ts'),
     output: {
-        path: _resolve(__dirname, 'build'),
-        filename: '[name].[contenthash]-bundle.js',
-        clean: true
+      path: _resolve(__dirname, 'build'),
+      filename: '[name].[contenthash]-bundle.js',
+      clean: true
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: _resolve(__dirname, 'public', 'index.html')
-        }),
-        new ProgressPlugin()
+      new HtmlWebpackPlugin({
+        template: _resolve(__dirname, 'public', 'index.html')
+      }),
+      isDev ? new ProgressPlugin() : undefined
     ],
     module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    }
-})
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    devtool: isDev && 'inline-source-map',
+    devServer: isDev
+      ? {
+          static: {
+            directory: _join(__dirname, 'public')
+          },
+          port: env.port ?? 3000,
+          open: true
+        }
+      : undefined
+  }
+}
