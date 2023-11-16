37a192286adce4929fa22ebf34e196750d3c325f
Nazhiganov AV <b3rt@yandex.ru>
2023-11-16 20:43:00 +0300

    Add webpack-html-plugin and progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {ProgressPlugin} = require('webpack')
 module.exports = (env)=> ({
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -7,5 +8,11 @@ module.exports = (env)=> ({
         path: path.resolve(__dirname, 'build'),
         filename: '[name].[contenthash]-bundle.js',
         clean: true
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        }),
+        new ProgressPlugin()
+    ]
 })
