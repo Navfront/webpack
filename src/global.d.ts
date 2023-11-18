fb89e7ce720d9c5f00cef05c9e40f61768395e62
Nazhiganov AV <b3rt@yandex.ru>
2023-11-18 22:38:49 +0300

    Add svgr/webpack loader

--- src/global.d.ts
+++ src/global.d.ts
@@ -7,6 +7,12 @@ declare module '*.css' {
   export = classNames
 }
 
+declare module '*.svg' {
+  import React from 'react'
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  export default SVG
+}
+
 declare module '*.jpg'
 declare module '*.png'
 declare module '*.webp'
--- src/pages/main/ui/index.tsx
+++ src/pages/main/ui/index.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
+import Icon from '@/assets/react.svg'
 
 const MainPage: FC = () => {
   return (
     <section>
-      <h1>Main Page</h1>
+      <h1>
+        Main Page <Icon width={50} fill={'red'} />
+      </h1>
+
       <Link to={'/one'}>Sub1</Link>
       <Link to={'/two'}>Sub2</Link>
       <Link to={'/about'}>To About Page</Link>
